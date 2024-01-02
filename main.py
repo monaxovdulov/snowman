a3ff24fb4881dec27bececfe65f0980edc19b312
Roman <35300872+monaxovdulov@users.noreply.github.com>
2024-01-02 15:51:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import turtle
+t = turtle.Turtle()
+t.shape('turtle')
+
+t.speed(7)
 
 screen = turtle.Screen()
 screen.bgcolor("sky blue")
-
+t.hideturtle()
 t.penup()
 
 t.goto(0, -210)
@@ -12,4 +17,32 @@ t.color('white')
 t.fillcolor('white')
 t.begin_fill()
 t.circle(90)
+
+
+t.goto(0, -90)
+t.circle(70)
+
+t.penup()
+t.goto(0, 30)
+t.circle(40)
+t.end_fill()
+t.color('black')
+t.goto(-15,80)
+
+t.pendown()
+t.circle(4)
+t.penup()
+
+t.goto(15,80)
+t.pendown()
+t.circle(4)
+
+t.penup()
+t.goto(0,65)
+t.pendown()
+
+t.color('red')
+t.fillcolor('orange')
+t.begin_fill()
+t.circle(4)
 t.end_fill()
