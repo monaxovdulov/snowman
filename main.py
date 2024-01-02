f94e2b32c064d3dd309e4c60155aec4c76b5c62f
Roman <35300872+monaxovdulov@users.noreply.github.com>
2024-01-02 15:31:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 
+screen = turtle.Screen()
+screen.bgcolor("sky blue")
+
+t.penup()
+
+t.goto(0, -210)
+
+
+t.pendown()
+t.color('white')
+t.fillcolor('white')
+t.begin_fill()
+t.circle(90)
+t.end_fill()
